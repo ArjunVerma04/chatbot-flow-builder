15756aebc5f0a913fa15da506c7c571ac9432643
ArjunVerma04 <arjunverma8352@gmail.com>
2025-07-27 21:27:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ReactFlowProvider } from 'reactflow';
 import FlowBuilder from './components/FlowBuilder';
-import './App.css';
+import './app.css';
 
 // This is the main App component
 function App() {
